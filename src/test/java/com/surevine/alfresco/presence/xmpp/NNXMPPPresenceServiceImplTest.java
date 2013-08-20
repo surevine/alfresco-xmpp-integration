78a3d75d99416cd25957137dcbcb596432935c6c
Simon White <simon.white@gmail.com>
2013-08-20 14:42:36 +0100

    Correct license headers

--- src/test/java/com/surevine/alfresco/presence/xmpp/NNXMPPPresenceServiceImplTest.java
+++ src/test/java/com/surevine/alfresco/presence/xmpp/NNXMPPPresenceServiceImplTest.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2008-2010 Surevine Limited.
+ *   
+ * Although intended for deployment and use alongside Alfresco this module should
+ * be considered 'Not a Contribution' as defined in Alfresco'sstandard contribution agreement, see
+ * http://www.alfresco.org/resource/AlfrescoContributionAgreementv2.pdf
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 package com.surevine.alfresco.presence.xmpp;
 
 import static org.junit.Assert.*;
@@ -58,14 +79,13 @@ public class NNXMPPPresenceServiceImplTest {
 	
 	@Test(expected=XMPPExecutionException.class)
 	public void testGetPresenceEmptyStringInTargetUser() {
-		SimplePresence presence = runGetPresenceTest("");
+		runGetPresenceTest("");
 	}
 	
 	@Test
 	public void testGetPresenceUnknownTargetUser() {
-		// TODO need to fix to be throwing exception for the correct reason... (that user doesn't exist...)
 		try {
-			SimplePresence presence = runGetPresenceTest("MrMagoo");
+			runGetPresenceTest("MrMagoo");
 			fail("An XMPPExecutionException was expected");
 		}
 		catch (XMPPExecutionException e) {
@@ -80,12 +100,8 @@ public class NNXMPPPresenceServiceImplTest {
 	
 	@Test
 	public void testGetPresenceTargetUserNotInRoster() {
-		SimplePresence presence = runGetPresenceTest("user0009-org02");
+		runGetPresenceTest("user0009-org02");
 	}	
-
-	// TODO add tests for if superuser not setup correctly...
-	// Can we instantiate impl fresh for each test? even if its singleton?
-	//
 
 	protected void runSetPresence(String userName, String password, SimplePresence.Availability availability, String superUserName, String superUserPassword) {
 		XMPPUser user = new XMPPUser(userName, password);
--- src/test/java/com/surevine/alfresco/presence/xmpp/NNXMPPUnreadMessageServiceImplTest.java
+++ src/test/java/com/surevine/alfresco/presence/xmpp/NNXMPPUnreadMessageServiceImplTest.java
@@ -1,3 +1,24 @@
+/*
+ * Copyright (C) 2008-2010 Surevine Limited.
+ *   
+ * Although intended for deployment and use alongside Alfresco this module should
+ * be considered 'Not a Contribution' as defined in Alfresco'sstandard contribution agreement, see
+ * http://www.alfresco.org/resource/AlfrescoContributionAgreementv2.pdf
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 package com.surevine.alfresco.presence.xmpp;
 
 import static org.junit.Assert.assertEquals;
@@ -7,13 +28,9 @@ import static org.junit.Assert.fail;
 import java.util.Iterator;
 
 import org.jivesoftware.smack.packet.Message;
-import org.jivesoftware.smack.packet.Presence;
 import org.jivesoftware.smackx.OfflineMessageHeader;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-
-import com.surevine.alfresco.presence.xmpp.SimplePresence.Availability;
 
 public class NNXMPPUnreadMessageServiceImplTest {
 
@@ -36,7 +53,6 @@ public class NNXMPPUnreadMessageServiceImplTest {
 				XMPPConfiguration.getConfiguration().setSuperUserPassword("T3G0gD1dJxp1sfko");
 				setXMPPConnectionProvider(connectionProvider);
 			}
-			// TODO How to avoid setting via the parent?
 			super.setUpRoster();
 		};
 		
